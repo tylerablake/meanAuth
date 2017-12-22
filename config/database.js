44d8f214ade057772fd6b9019083429e1c69565d
Tyler Blake <tyler.blake.louisville@gmail.com>
2017-12-21 22:03:24 -0500

    test

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    uri: 'mongodb://user:test@ds161426.mlab.com:61426/meanauth',
+    uri: 'youWish ',
     options: { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } }
             },
