6456800459aba5b5c72b27079b974fa424119c09
Tyler Blake <tyler.blake.louisville@gmail.com>
2017-12-22 22:41:46 -0500

    test GitHub CI deployment

--- config/database.js
+++ config/database.js
@@ -3,5 +3,5 @@ module.exports = {
     options: { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
                 replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } }
             },
-    secret: 'yourSecretToken'
+    secret: 'yourSecret'
 }
